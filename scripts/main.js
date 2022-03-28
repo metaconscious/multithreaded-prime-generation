30bd45505ff65c93b91b1a2397f76783cd9485d1
Marko <94597129+metaconscious@users.noreply.github.com>
2022-03-28 21:33:41 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const worker = new Worker(`scripts/generate.js`);
+
+document.querySelector('#generate').addEventListener('click', () => {
+    const quota = document.querySelector('#quota').value;
+    worker.postMessage({
+        command: 'generate',
+        quota: quota
+    });
+});
+
+worker.addEventListener('message', message => {
+    document.querySelector('#output')
+        .textContent = `Finished generating ${message.data} primes!`;
+});
+
+document.querySelector('#reload').addEventListener('click', () => {
+    document.querySelector('#user-input').value = 'Try typing in here immediately after pressing "Generate primes"';
+    document.location.reload();
+});
